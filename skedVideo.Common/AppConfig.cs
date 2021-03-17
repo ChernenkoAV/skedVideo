36191138c26d4617a5cbc84e079c4003062afc9d
ChernenkoAV <alexej.chernenko@gmail.com>
2021-03-17 22:53:44 +0300

    Что-то не совсем полезное.

---
+++ skedVideo.Common/AppConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace skedVideo
+{
+    public static class AppConfig
+    {
+        static AppConfig()
+        {
+
+        }
+
+        /// <summary>
+        /// Путь к 
+        /// </summary>
+        public static string path_to_logo
+        {
+            get; private set;
+        }
+
+
+    }
+}
